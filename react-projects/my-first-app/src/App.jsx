d9377265a28bb65ea722c41a6f6adfa20f691aca
shruthi06071995 <shruthiganesan518@gmail.com>
2026-01-08 21:12:54 +0530

    new change

---
+++ react-projects/my-first-app/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Home from "./Home";
+
+
+function App() {
+  return(
+    <div className='container'>
+      <h1>New Page</h1>
+      <p>Sample Content</p>
+      <Home/>
+      <Home/>
+      <Home/>
+    </div>
+  )
+}
+export default App;
---
+++ react-projects/my-first-app/src/Home.jsx
@@ -0,0 +1,11 @@
+
+
+function Home() {
+    return(
+        <div className="box">
+            <h2>Hello World!</h2>
+            <p>Sample Content HI!</p>
+        </div>
+    )
+}
+export default Home;
